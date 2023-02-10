89828de9f08aa38a80418b8edcc9471ad917b4a5
Obaydullah <obaydullah.2041@gmail.com>
2023-02-10 19:21:15 +0600

    my first golang programe

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import ("fmt")
+
+func main(){
+	
+	fmt.Println("this message will be printed with a linefeed a the end")
+	var name string = "Obaydullah"
+	fmt.Println(name)
+
+	rune1 := 'B'
+	fmt.Println(rune1)
+
+	var chr rune = 'A'
+	fmt.Println(chr)
+
+
+
+}
